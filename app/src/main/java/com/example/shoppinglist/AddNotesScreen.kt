41ff919a9536869db19e682d02d15dc56fcb9ef8
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2024-12-27 23:21:49 +0530

    UI

--- app/src/main/java/com/example/shoppinglist/AddNotesScreen.kt
+++ app/src/main/java/com/example/shoppinglist/AddNotesScreen.kt
@@ -1,15 +1,22 @@
 package com.example.shoppinglist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -20,6 +27,11 @@ fun NewNoteScreen(){
         
         topBar = {
             TopAppBar(
+
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFFFFEE9),
+                ),
+
                 navigationIcon = {
                     IconButton(onClick = { /*TODO*/ }) {
                         Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
@@ -34,7 +46,13 @@ fun NewNoteScreen(){
         },
 
         content = {
-            paddingValues ->  
+            paddingValues ->
+            Column(modifier = Modifier
+                .padding()
+                .background(Color(0xFFFFFEE9))
+                .fillMaxSize()) {
+
+            }
         }
         
     )
